c7a472070e82543e3db838f6c5f1a3ca59d34c79
theuranus <theuranus.ntb@gmail.com>
2018-10-09 15:06:45 +0700

    complete calc

--- SimpleCalculator/calc2.js
+++ SimpleCalculator/calc2.js
@@ -1,20 +1,34 @@
 (function() {
 
-    const calc = document.getElementById('calc')
-    let operation = ''
+    const calc = document.getElementById('calc');
+    const screen = document.getElementById('screen');
+    const report = document.getElementById('report');
+    let operation = '';
+    let result = 0;
+    report.value = '';
 
     calc.addEventListener('click', function(e) {
-       if (e.target.matches('input[type="button"][value]')) {
-        const val = e.target.value
-        if (val === '=') {
-            const result = eval(operation)
-            console.log(result)
-        } else {
-            operation += val
+        if (e.target.matches('input[type="button"][value]')) {
+            const val = e.target.value;
+            switch(val) {
+                case '=': {
+                    result = eval(operation);
+                    screen.value = result;
+                    report.value += operation + '=' + result + '\n';
+                    break;
+                }
+                case 'AC': {
+                    screen.value = '';
+                    operation = '';
+                    result = 0;
+                    break;
+                }
+                default: {
+                    operation += val;
+                    screen.value = operation;
+                    break;
+                }
+            }   
         }
-       } else {
-           console.log('do nothing')
-       }
     })
-
 })()
